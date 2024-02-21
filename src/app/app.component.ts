b6ee72a316791db23adb03d69294f6d391819c75
KianGillespie03 <kiangillespie03@gmail.com>
2024-02-21 12:35:55 +0000

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { ListMoviesComponent } from './list-movies/list-movies.component';
+import { MovieService } from './movie.service';
+import { Movie } from './movie.model';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, AddMovieComponent, ListMoviesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Week5movie-list';
+  title = 'Week 5 movie-list';
+
+  movies:any[]=[];
+
+  constructor(private movieService:MovieService) {}
+
+  ngOnInIt() {
+    this.movies=this.movieService.getMovies();
+    console.log(this.movies);
+  }
+
+  public mySelectedMovie!:Movie;
+
+  setSelectedMovie(movie:Movie) {
+    this.mySelectedMovie=movie;
+  }
 }
